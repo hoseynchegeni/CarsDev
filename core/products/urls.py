04883e47fcbe9aa2b60ebe8a3a23f8f41eed905c
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-03-19 03:21:12 +0330

    build car list and product  list

--- core/products/urls.py
+++ core/products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CarDetailView, CategoryView
+from .views import CarDetailView, CategoryView, ProductsView, CarListView
 app_name = 'products'
 
 urlpatterns = [
+    path('cars/',CarListView.as_view(),name='cars'),
     path('cars/<int:pk>/', CarDetailView.as_view(),name='CarDetail'),
     path('category/<int:id>/', CategoryView.as_view(),name='category'),
+    path('products/', ProductsView.as_view(), name='products')
 ]
--- core/products/views.py
+++ core/products/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView,ListView
 from django.views import View 
 from .models import Car, ProductCategory, Product
 # Create your views here.
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.all()
+    context_object_name = 'cars'
+
 class CarDetailView(DetailView):
     model = Car
 
@@ -10,3 +15,8 @@ class CategoryView(View):
     def get(self, request, id):
         obj = Product.objects.filter(category_id = id)
         return render(request, 'products/category.html', {'obj':obj})
+    
+class ProductsView(ListView):
+    model = Product
+    queryset = Product.objects.filter(status = True)
+    context_object_name = "products"
