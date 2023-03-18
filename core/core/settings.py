b1f6fd29a85fd3ceffedc4d3587309ddc661c3c4
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-03-18 20:03:01 +0330

    setpu template folder

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local
+    'blog',
+    'products',
+    # Third Party Apps
+    'colorfield',
+    'djmoney',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,9 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
